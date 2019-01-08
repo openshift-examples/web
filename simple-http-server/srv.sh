b3c0d77e15394fee2269d45c72e1dbaede968339
Robert Bohne <robert.bohne@redhat.com>
2019-01-08 09:11:24 +0100

    Add some informations to simple-http-server

--- simple-http-server/srv.sh
+++ simple-http-server/srv.sh
@@ -5,10 +5,16 @@ echo -e "\r"
 
 echo "# Basic POD Informations"
 echo -e "\n## Env"
-env
+env | sort
 
-echo -e "\n## ip link"
-ip link
+echo -e "\n## ip addr show"
+ip addr show
+
+echo -e "\n## ip route show"
+ip route show
+
+echo -e "\n## resolv.conf"
+cat /etc/resolv.conf
 
 echo -e "\n## request"
 read request
