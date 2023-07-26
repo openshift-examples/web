2e0be581b3fc3d44f681d5d27abd2908335ba921
Robert Bohne <robert.bohne@redhat.com>
2023-07-27 00:01:41 +0200

    Fixed markdown lint

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -2,6 +2,8 @@ all
 # our changelog does this, by design
 # MD013 - Line length
 exclude_rule 'MD013'
+# MD041 First line in file should be a top level header
+exclude_rule 'MD041'
 # default in next version, remove then
 #rule 'MD007', :indent => 3
 
