b755e0307d1963821597d00e3da4c209241a8781
Robert Bohne <robert.bohne@redhat.com>
2025-01-02 18:05:05 +0100

    Improve localk pre-commit run

--- helper/run-pre-commit.sh
+++ helper/run-pre-commit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-git config --global --add safe.directory /opt/app-root/src
+export GIT_CONFIG_COUNT=1
+export GIT_CONFIG_KEY_0=safe.directory 
+export GIT_CONFIG_VALUE_0=/opt/app-root/src
 
 pre-commit run --color=never --from-ref main --to-ref HEAD
