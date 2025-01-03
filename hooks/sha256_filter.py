3be0ce2e2c2f84ad5a900fa5b1b10d02f3dd3aec
Robert Bohne <robert.bohne@redhat.com>
2025-01-03 10:30:46 +0100

    Delete useless hooks/

--- hooks/sha256_filter.py
+++
@@ -1,9 +0,0 @@
-
-import hashlib
-
-def sha256(x):
-    return hashlib.sha256(x.encode('utf-8')).hexdigest()
-
-def on_env(env, config, files, **kwargs):
-    env.filters['sha256'] = sha256
-    return env
