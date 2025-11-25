1d185c74a8df3e2981a0e25697d5628107eb541d
Robert Bohne <robert.bohne@redhat.com>
2025-11-25 11:34:40 +0100

    Update local run stuff

--- run-local-pre-commit.sh
+++ run-local-pre-commit.sh
@@ -1,3 +1,3 @@
 podman run -ti --rm -v \
-    $(pwd):/opt/app-root/src quay.io/openshift-examples/builder:devel \
+    $(pwd):/opt/app-root/src quay.io/openshift-examples/builder:202511251107 \
     ./helper/run-pre-commit.sh
