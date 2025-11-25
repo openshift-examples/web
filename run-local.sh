71bd01782086196d6ed74f32e63a3848371a541d
Robert Bohne <robert.bohne@redhat.com>
2025-11-25 11:34:40 +0100

    Update local run stuff

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
 podman run -ti --user 0 --rm \
   -v $(pwd):/opt/app-root/src:z \
-  -p 8080:8080 quay.io/openshift-examples/builder:devel
+  -p 8080:8080 quay.io/openshift-examples/builder:202511251107
 
