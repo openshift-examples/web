98f9257086cb025ec02a1317dc4c5873bb1a1e9f
Robert Bohne <robert.bohne@redhat.com>
2026-01-12 17:26:19 +0100

    Open all links in a new tab

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
 podman run -ti --user 0 --rm \
   -v $(pwd):/opt/app-root/src:z \
-  -p 8080:8080 quay.io/openshift-examples/builder:202511251107
+  -p 8080:8080 quay.io/openshift-examples/builder:202601121657
 
